df1db547e45085aa91bc89574a4b5f212f194bdb
RyanRillema <ryanrillema@gmail.com>
2024-11-30 11:39:06 +0200

    Add project files.

---
+++ Unboxer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Unboxer.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Unboxer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Unboxer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
